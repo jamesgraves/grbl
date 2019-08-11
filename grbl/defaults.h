02d40c7fd69f1961695761fe7ddc4c70919beb79
James Graves <james.c.graves.jr@gmail.com>
2019-08-11 07:58:16 -0500

    Set defaults for MB XY plotter v1p5.

--- grbl/defaults.h
+++ grbl/defaults.h
@@ -529,6 +529,57 @@
   #define DEFAULT_HOMING_PULLOFF 1.0 // mm
 #endif
 
+#ifdef DEFAULTS_MAKEBLOCK_XY_PLOTTER_V1P5
+  // Settings for hacked Makeblock XY Plotter v1 v2 hybrid.
+  // Timing belt: 2.032mm spacing (25 per 2 inches).
+  #define BELT_SPACING 2.032
+
+  // X
+  #define MICROSTEPS_X 2
+  #define STEPS_PER_REV_X 200.0
+  #define MM_PER_REV_X (BELT_SPACING * 18.0) /* 18T pulley */
+  #define DEFAULT_X_STEPS_PER_MM (STEPS_PER_REV_X * MICROSTEPS_X / MM_PER_REV_X)
+
+  // Y
+  #define MICROSTEPS_Y 8
+  #define STEPS_PER_REV_Y 200.0
+  #define MM_PER_REV_Y (BELT_SPACING * 90.0) /* 18T pulley */
+  #define DEFAULT_Y_STEPS_PER_MM (STEPS_PER_REV_Y * MICROSTEPS_Y / MM_PER_REV_Y)
+
+  #define DEFAULT_Z_STEPS_PER_MM 250.0 /* no actual z axis */
+  #define DEFAULT_X_MAX_RATE 2000.0 // mm/min
+  #define DEFAULT_Y_MAX_RATE 2000.0 // mm/min
+  #define DEFAULT_Z_MAX_RATE 2000.0 // mm/min
+  #define DEFAULT_X_ACCELERATION (10.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+  #define DEFAULT_Y_ACCELERATION (10.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+  #define DEFAULT_Z_ACCELERATION (10.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+  #define DEFAULT_X_MAX_TRAVEL 390.0 // mm NOTE: Must be a positive value.
+  #define DEFAULT_Y_MAX_TRAVEL 320.0 // mm NOTE: Must be a positive value.
+  #define DEFAULT_Z_MAX_TRAVEL  10.0 // mm NOTE: Must be a positive value.
+  #define DEFAULT_SPINDLE_RPM_MAX 1000.0 // rpm
+  #define DEFAULT_SPINDLE_RPM_MIN 0.0 // rpm
+  #define DEFAULT_STEP_PULSE_MICROSECONDS 10
+  #define DEFAULT_STEPPING_INVERT_MASK 0
+  #define DEFAULT_DIRECTION_INVERT_MASK 0
+  #define DEFAULT_STEPPER_IDLE_LOCK_TIME 25 // msec (0-254, 255 keeps steppers enabled)
+  #define DEFAULT_STATUS_REPORT_MASK 1 // MPos enabled
+  #define DEFAULT_JUNCTION_DEVIATION 0.01 // mm
+  #define DEFAULT_ARC_TOLERANCE 0.002 // mm
+  #define DEFAULT_REPORT_INCHES 0 // false
+  #define DEFAULT_INVERT_ST_ENABLE 0 // false
+  #define DEFAULT_INVERT_LIMIT_PINS 0 // false
+  #define DEFAULT_SOFT_LIMIT_ENABLE 0 // false
+  #define DEFAULT_HARD_LIMIT_ENABLE 0  // false
+  #define DEFAULT_INVERT_PROBE_PIN 0 // false
+  #define DEFAULT_LASER_MODE 1 // false
+  #define DEFAULT_HOMING_ENABLE 1  // false
+  #define DEFAULT_HOMING_DIR_MASK 0 // move positive dir
+  #define DEFAULT_HOMING_FEED_RATE 25.0 // mm/min
+  #define DEFAULT_HOMING_SEEK_RATE 500.0 // mm/min
+  #define DEFAULT_HOMING_DEBOUNCE_DELAY 250 // msec (0-65k)
+  #define DEFAULT_HOMING_PULLOFF 1.0 // mm
+#endif
+
 #ifdef DEFAULTS_SIMULATOR
   // Settings only for Grbl Simulator (www.github.com/grbl/grbl-sim)
   // Grbl generic default settings. Should work across different machines.
