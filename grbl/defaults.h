3b7006bcf4e249297ca4c36a52fc73638ec0b08f
James Graves <james.c.graves.jr@gmail.com>
2019-08-12 09:52:44 -0500

    Various updates.  Give more margin for travel.
    Need to switch polarity on Y back to normal, we can invert
    direction in software.  Also change homing setup and direction.
    
    On X, homing goes toward positive X direction.
    On Y, homing goes toward negagive Y direction.
    
    Reduced soft limit area, and turned that on by default.

--- grbl/defaults.h
+++ grbl/defaults.h
@@ -535,13 +535,13 @@
   #define BELT_SPACING 2.032
 
   // X
-  #define MICROSTEPS_X 2
+  #define MICROSTEPS_X 8
   #define STEPS_PER_REV_X 200.0
   #define MM_PER_REV_X (BELT_SPACING * 18.0) /* 18T pulley */
   #define DEFAULT_X_STEPS_PER_MM (STEPS_PER_REV_X * MICROSTEPS_X / MM_PER_REV_X)
 
   // Y
-  #define MICROSTEPS_Y 8
+  #define MICROSTEPS_Y 32
   #define STEPS_PER_REV_Y 200.0
   #define MM_PER_REV_Y (BELT_SPACING * 90.0) /* 18T pulley */
   #define DEFAULT_Y_STEPS_PER_MM (STEPS_PER_REV_Y * MICROSTEPS_Y / MM_PER_REV_Y)
@@ -553,14 +553,14 @@
   #define DEFAULT_X_ACCELERATION (10.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
   #define DEFAULT_Y_ACCELERATION (10.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
   #define DEFAULT_Z_ACCELERATION (10.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
-  #define DEFAULT_X_MAX_TRAVEL 390.0 // mm NOTE: Must be a positive value.
-  #define DEFAULT_Y_MAX_TRAVEL 320.0 // mm NOTE: Must be a positive value.
+  #define DEFAULT_X_MAX_TRAVEL 380.0 // (actual max 390) mm NOTE: Must be a positive value.
+  #define DEFAULT_Y_MAX_TRAVEL 310.0 // (actual max 320) mm NOTE: Must be a positive value.
   #define DEFAULT_Z_MAX_TRAVEL  10.0 // mm NOTE: Must be a positive value.
   #define DEFAULT_SPINDLE_RPM_MAX 1000.0 // rpm
   #define DEFAULT_SPINDLE_RPM_MIN 0.0 // rpm
   #define DEFAULT_STEP_PULSE_MICROSECONDS 10
   #define DEFAULT_STEPPING_INVERT_MASK 0
-  #define DEFAULT_DIRECTION_INVERT_MASK 1
+  #define DEFAULT_DIRECTION_INVERT_MASK 0x02   // b00000zyx  +x, -y, +z
   #define DEFAULT_STEPPER_IDLE_LOCK_TIME 25 // msec (0-254, 255 keeps steppers enabled)
   #define DEFAULT_STATUS_REPORT_MASK 1 // MPos enabled
   #define DEFAULT_JUNCTION_DEVIATION 0.01 // mm
@@ -568,12 +568,12 @@
   #define DEFAULT_REPORT_INCHES 0 // false
   #define DEFAULT_INVERT_ST_ENABLE 0 // false
   #define DEFAULT_INVERT_LIMIT_PINS 0 // false
-  #define DEFAULT_SOFT_LIMIT_ENABLE 0 // false
+  #define DEFAULT_SOFT_LIMIT_ENABLE 1 // true
   #define DEFAULT_HARD_LIMIT_ENABLE 0  // false
   #define DEFAULT_INVERT_PROBE_PIN 0 // false
-  #define DEFAULT_LASER_MODE 1 // false
+  #define DEFAULT_LASER_MODE 1 // true
   #define DEFAULT_HOMING_ENABLE 1  // false
-  #define DEFAULT_HOMING_DIR_MASK 0 // move positive dir
+  #define DEFAULT_HOMING_DIR_MASK 0x02 // b00000zyx  x, -y, z
   #define DEFAULT_HOMING_FEED_RATE 25.0 // mm/min
   #define DEFAULT_HOMING_SEEK_RATE 500.0 // mm/min
   #define DEFAULT_HOMING_DEBOUNCE_DELAY 250 // msec (0-65k)
